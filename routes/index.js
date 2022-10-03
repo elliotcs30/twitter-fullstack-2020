985c510267d1ce4eda7fe40b5e70877e37a7f0af
Elliot <elliotcs30@gmail.com>
2022-10-03 17:21:04 +0800

    feat: add signup page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const twitterController = require('../controllers/twitter-controller')
 const admin = require('./modules/admin')
+const twitterController = require('../controllers/twitter-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
 router.get('/twitters', twitterController.getTwitters)
 router.use('/', (req, res) => res.redirect('/twitters'))
 
